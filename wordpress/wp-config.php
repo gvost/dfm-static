ef666907e06f18466e3e51395a9fb6f5c6f8cee2
David Johnson <davidjohnson@repeaterRifle.local>
2015-01-24 20:21:35 -0800

    added wp-config.php to ignore

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'dfm_wp');
 
 /** MySQL database username */
-define('DB_USER', 'simstats_jacob');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'jm112358');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
