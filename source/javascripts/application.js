acf9aca6223999258ed5c2fe4d04e69265c8d2b1
Jake Mensch <jake.mensch@gmail.com>
2015-01-28 09:01:47 -0800

    commenting

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,1 +1,282 @@
-//= require_tree .
+/*
+      __         __   ___       
+|  | |__)   +   |  \ |__   |\/| 
+\__/ |__)       |__/ |     |  | 
+
+copyright: 2015
+*/
+
+
+/*
+
+This file contains all of the site JS except the scroller.  The scroller
+is included via the require _scroller directive below.
+
+*/
+
+
+//= require _scroller
+
+$(window).load(function() {
+
+  console.log('♥︎ upstartBureau');
+  
+  ////////// ANIMATE PAGE LOADS ///////////
+
+  // remove cover and temp images
+  $("#cover").remove();
+  $(".temp-image").remove();
+
+  // make the header full opacity and slide away the loading page
+  $("#header").css("opacity", "1");
+  $("#loading-page").animate({
+    'left': '100%',
+    'right': '-100%',
+    'opacity': '0.5'
+  }, 500, function() {
+    $(this).remove();
+  });
+
+});
+
+
+$(document).ready(function() {
+
+  //////////// LOAD RANDOM CANDLE IMAGE ///////////
+
+  var img_classes = [
+    'candle-smoke',
+    'candle-flame'
+  ];
+  var img_class = img_classes[Math.floor(Math.random() * img_classes.length)];
+  $('.feature-image').addClass(img_class);
+
+  ///////////// ANIMATE LOADING BAR ///////////////
+
+  // collect the background images
+  var back_images = $('*').filter(function() {
+    return $(this).css("background-image") != "none";
+  });
+
+  // add all of them to hidden divs
+  back_images.each(function() {
+    var url = $(this).css("background-image").replace("url(", "").replace(")", "");
+    $("body").append('<img class="temp-image" src="' + url + '" style="display:none;">');
+  });
+
+  // collect all images (both the new hidden ones and the ones with original 'img' tags)
+  var images = $("img");
+  var image_count = images.length;
+
+  // cover and lighten the header
+  var $cover = $('<div id="cover">').css({
+    "background-color": "white",
+    "position": "absolute",
+    "top": "0",
+    "bottom": "0",
+    "right": "0",
+    "left": "0"
+  });
+  $("#header").append($cover).css("background-color", "black").css("opacity", "0.5");
+
+  // remove the cover as images are loaded
+  var new_width, c_width = $cover.width();
+  images.each(function(index) {
+    $(this).load(function() {
+      new_width = ((index + 1) * (c_width / image_count));
+
+      $cover.animate({
+        "left": new_width + 'px'
+      }, 500);
+    });
+  });
+
+  /////////// SHOW/HIDE THE NAV PAGE //////////
+
+  $("#nav-toggle").click(function() {
+    
+    var $nav_page = $("#nav-page");
+
+    if ($nav_page.css("left") != "0px") {
+
+      $nav_page.animate({
+        left: 0,
+        opacity: 1
+      }, {
+        duration: 300,
+        easing: 'easeInOutCubic'
+      });
+
+      setTimeout(function(){
+        $("#nav-toggle").text("Close");
+      }, 300);
+
+    } else {
+
+      $nav_page.animate({
+        left: "100%",
+        opacity: 0
+      }, {
+        duration: 600,
+        easing: 'easeInOutCubic'
+      });
+
+      setTimeout(function(){
+        $("#nav-toggle").text("Explore");
+      }, 600);
+
+    }
+  });
+
+  ////////// HOME PAGE HOVER STATES ///////////
+
+  var fading;
+  $(".hover").hover(function() {
+    if (!fading) {
+      $(this).children(".image").fadeOut(300);
+      fading = true;
+    }
+  }, function() {
+    $(this).children(".image").fadeIn(300);
+    fading = false;
+  });
+
+  /////////// HOME PAGE ABOUT-BLURB SLIDER ///////////////
+
+  var $slider = $('.slider');
+  var $slides = $('.slide');
+  var blurbTimer = null;
+
+  // show the first slide
+  $slides.first().addClass('active').fadeIn(300);
+
+  // left clicks
+  $('#left-button').click(function() {
+    var i = $slider.find('.active').index();
+
+    $slides.eq(i).removeClass('active').fadeOut(300);
+    $slides.eq(i - 1).addClass('active').fadeIn(300);
+
+    initAutoClick();
+  });
+
+  // right clicks
+  $('#right-button').click(function() {
+    var i = $slider.find('.active').index();
+    var next_slide = (i + 1) % $slides.length;
+
+    $slides.eq(i).removeClass('active').fadeOut(300);
+    $slides.eq(next_slide).addClass('active').fadeIn(300);
+
+    initAutoClick();
+  });
+
+  function initAutoClick() {
+    // auto-switch slides every 6 seconds
+    clearInterval(blurbTimer);
+    blurbTimer = setInterval(function() {
+      $('#right-button').trigger('click');
+    }, 6000);
+  }
+
+  // stop the timer whenever a link is clicked
+  $("a").click(function() {
+    clearInterval(blurbTimer);
+  });
+
+  // start the auto-switch
+  initAutoClick();
+
+  /////////// NAV PAGE LINKS ///////////////
+
+  $('.page-links').click(function(e) { 
+
+    var url = $(this).attr('href');
+
+    if (!$(this).hasClass('no-prevent')) {
+      e.preventDefault();
+    
+      $('body').animate({
+        'opacity': '0',
+        'left': '-100%'
+      }, 300, 'easeOutQuad');
+
+      setTimeout(function() {
+        window.location = url;
+      }, 300);  
+    }
+  });
+
+  ////////// HOME ICON CLICK ON ALL PAGES /////////////
+  
+  $('.to-home').click(function(e) {
+    var url = $(this).attr('href');
+
+    e.preventDefault();
+    $('body').animate({
+      'opacity': '0',
+      'left': '100%'
+    }, 300, 'easeOutQuad');
+
+    setTimeout(function(){
+      window.location = url;
+    }, 300);
+  });
+
+  //////////////////// PARALLAX  //////////////////
+
+  $('body').scroll(function(e){
+
+    var $content = $(".content");
+
+    // if the current page has a content section...
+    if ($content.length) {
+
+      // move the top-background and insert outline
+      // 0.1 and 0.3 control the relative speeds of the background and insert-outline
+      // -100 is the initial 'top' of the insert-outline
+      var scrolled = $content.offset().top;
+      var initHeight = $(".top-background").height();
+      $('.top-background').css('top', ((scrolled - initHeight) * 0.1) + 'px');
+      $('.top-insert-outline').css('top', (-100 + ((scrolled - initHeight) * 0.3)) + 'px');
+      
+      // display / hide the top-background image 
+      // depending on how far we've scrolled
+      if (scrolled < 0) {
+        $('.top-background').css('display', 'none');
+        $('.parallax-background').css('display', 'block');
+      } else {
+        $('.top-background').css('display', 'block');
+        $('.parallax-background').css('display', 'none');
+      }
+    }
+    
+  });
+
+  ///////////////// CONTACT FORM //////////////////////
+
+  $("#contact-submit").click(function() {
+
+    $("#server-response").text("Sending message...");
+
+    $.ajax({
+      type:  'post',
+      url:   '/php/contact_submit.php',
+      data:  {
+        firstname:   $("#contact-first-name").val(),
+        lastname:    $("#contact-last-name").val(),
+        email:       $("#contact-email").val(), 
+        phone:       $("#contact-phone").val(),
+        project:     $("#contact-project").val(),
+        find:        $("#contact-find-us").val()
+      },
+      success: function(response) {
+        setTimeout(function() {
+          $("#server-response").text(response);         
+        }, 500);
+      }
+    }); 
+
+  });
+
+});
