a0fffc73e47955130d5cdc793f7d894d6361fc09
davieghost <davidjohnson1297@gmail.com>
2015-01-29 14:12:35 -0800

    everything up to date and synced

--- build/javascripts/application.js
+++ build/javascripts/application.js
@@ -7,6 +7,141 @@ copyright: 2015
 */
 
 
+/*
+
+The script animates elements as the page scrolls. To animate any element, add one of these 
+classes to the element in the html.
+
+1. scroll-fast, scroll-medium, scroll-slow
+  - these classes will move the element up into the window when the user is scrolling down,
+  and down into the window when the user is scrolling up
+
+2. scroll-fast-left, scroll-medium-left, scroll-slow-left
+  - these classes will slide the element in from the right side of the page when it enters 
+  the window (regardless of which direction the user is scrolling)
+
+3. scroll-fast-right, scroll-medium-right, scroll-slow-right
+  - these classes will slide the element in from the left side of the page when it enters 
+  the window (regardless of which direction the user is scrolling)
+
+Basically, the script works by adding a class called 'move-up', 'move-down', 'move-left', or
+'move-right' when the element is outside the window, and removing that class when the element 
+enters the window (thus restoring the element to its original position).  
+
+All of the 'scroll' and 'move' classes are defined in stylesheets/_scroller.scss.
+
+*/
+
+
+
+$(document).ready(function() {
+
+  // to hold all of the scrollable elements
+  var elements = [];
+
+  // for each of the scrollable elements...
+  $('.scroll-fast, .scroll-fast-left, .scroll-fast-right, ' + 
+    '.scroll-medium, .scroll-medium-left, .scroll-medium-right, ' + 
+    '.scroll-slow, .scroll-slow-left, .scroll-slow-right').each(function() {
+
+    var elem = $(this);
+
+    // set the scrollStyle property of the element
+    if (elem.hasClass('scroll-fast-left') || 
+        elem.hasClass('scroll-medium-left') ||
+        elem.hasClass('scroll-slow-left'))
+      elem.scrollStyle = 'left';
+    else if (elem.hasClass('scroll-fast-right') || 
+             elem.hasClass('scroll-medium-right') ||
+             elem.hasClass('scroll-slow-right'))
+      elem.scrollStyle = 'right';
+    else
+      elem.scrollStyle = 'normal';
+
+    // and add the element to the array
+    elements.push(elem);
+
+  });
+
+  // when the page scrolls, animate the elements depending on the scroll speed and style
+  $('body').scroll(function(e) {
+
+    for (var i = 0; i < elements.length; i++) {
+
+      switch(elements[i].scrollStyle) {
+        case 'normal':
+          switch(elementPosition(elements[i])) {
+            case 'above':
+              elements[i].addClass('move-up');
+              break;
+            case 'below':
+              elements[i].addClass('move-down');
+              break;
+            case 'within':
+              elements[i].removeClass('move-down').removeClass('move-up');
+              break;
+          }   
+          break; 
+
+        case 'left':
+          switch(elementPosition(elements[i])) {
+            case 'above': 
+            case 'below':
+              elements[i].addClass('move-right');
+              break;
+            case 'within':
+              elements[i].removeClass('move-right');
+              break;
+          } 
+          break;
+
+        case 'right':
+          switch(elementPosition(elements[i])) {
+            case 'above':
+            case 'below':
+              elements[i].addClass('move-left');
+              break;
+            case 'within':
+              elements[i].removeClass('move-left');
+              break;
+          } 
+          break; 
+      }
+
+    }
+  });
+
+  // determines whether the given element is above, below, or within the window
+  function elementPosition(elem) {
+    if (elem.offset().top + elem.height() < $(window).scrollTop()) {
+      return 'above';
+    } else if (elem.offset().top > $(window).scrollTop() + $(window).height()) {
+      return 'below';
+    } else {
+      return 'within';
+    }
+  }
+
+});
+/*
+      __         __   ___       
+|  | |__)   +   |  \ |__   |\/| 
+\__/ |__)       |__/ |     |  | 
+
+copyright: 2015
+*/
+
+
+/*
+
+This file contains all of the site JS except the scroller.  The scroller
+is included via the require _scroller directive below.
+
+*/
+
+
+
+
 $(window).load(function() {
 
   console.log('♥︎ upstartBureau');
@@ -70,17 +205,14 @@ $(document).ready(function() {
   $("#header").append($cover).css("background-color", "black").css("opacity", "0.5");
 
   // remove the cover as images are loaded
-  var c_width = $cover.width();
-  var i = 0, new_width;
-  images.each(function() {
+  var new_width, c_width = $cover.width();
+  images.each(function(index) {
     $(this).load(function() {
-      new_width = ((i + 1) * (c_width / image_count));
+      new_width = ((index + 1) * (c_width / image_count));
 
       $cover.animate({
         "left": new_width + 'px'
       }, 500);
-
-      i++;
     });
   });
 
@@ -103,9 +235,6 @@ $(document).ready(function() {
       setTimeout(function(){
         $("#nav-toggle").text("Close");
       }, 300);
-      
-      // prevent scrolling in the background when nav is open
-      $("body").css("overflow", "hidden");
 
     } else {
 
@@ -121,8 +250,6 @@ $(document).ready(function() {
         $("#nav-toggle").text("Explore");
       }, 600);
 
-      // restore scrolling
-      $("body").css("overflow", "auto"); 
     }
   });
 
@@ -154,6 +281,8 @@ $(document).ready(function() {
 
     $slides.eq(i).removeClass('active').fadeOut(300);
     $slides.eq(i - 1).addClass('active').fadeIn(300);
+
+    initAutoClick();
   });
 
   // right clicks
@@ -163,17 +292,25 @@ $(document).ready(function() {
 
     $slides.eq(i).removeClass('active').fadeOut(300);
     $slides.eq(next_slide).addClass('active').fadeIn(300);
+
+    initAutoClick();
   });
 
-  // auto-switch slides every 6 seconds
-  blurbTimer = setInterval(function() {
-    $('#right-button').trigger('click');
-  }, 6000);
+  function initAutoClick() {
+    // auto-switch slides every 6 seconds
+    clearInterval(blurbTimer);
+    blurbTimer = setInterval(function() {
+      $('#right-button').trigger('click');
+    }, 6000);
+  }
 
   // stop the timer whenever a link is clicked
   $("a").click(function() {
     clearInterval(blurbTimer);
   });
+
+  // start the auto-switch
+  initAutoClick();
 
   /////////// NAV PAGE LINKS ///////////////
 
@@ -217,14 +354,20 @@ $(document).ready(function() {
 
     var $content = $(".content");
 
+    // if the current page has a content section...
     if ($content.length) {
-      var scrolled = $content.offset().top;
-      var topper = scrolled + $(window).height();
 
-      $('.top-background').css('top', ((scrolled * 0.1) - 80) + 'px');
-      $('.top-insert-outline').css('top', ((scrolled * 0.3) - 350) + 'px');
+      // move the top-background and insert outline
+      // 0.1 and 0.3 control the relative speeds of the background and insert-outline
+      // -100 is the initial 'top' of the insert-outline
+      var scrolled = $content.offset().top;
+      var initHeight = $(".top-background").height();
+      $('.top-background').css('top', ((scrolled - initHeight) * 0.1) + 'px');
+      $('.top-insert-outline').css('top', (-100 + ((scrolled - initHeight) * 0.3)) + 'px');
       
-      if (topper <= $(window).scrollTop()) {
+      // display / hide the top-background image 
+      // depending on how far we've scrolled
+      if (scrolled < 0) {
         $('.top-background').css('display', 'none');
         $('.parallax-background').css('display', 'block');
       } else {
@@ -239,147 +382,26 @@ $(document).ready(function() {
 
   $("#contact-submit").click(function() {
 
-    var info = {
-      firstname:   $("#contact-first-name").val(),
-      lastname:    $("#contact-last-name").val(),
-      email:       $("#contact-email").val(), 
-      phone:       $("#contact-phone").val(),
-      project:     $("#contact-project").val(),
-      find:        $("#contact-find-us").val()
-    };
-
     $("#server-response").text("Sending message...");
 
     $.ajax({
-      type:   'post',
-      url:    'http://localhost/DFM/DFM_static/php/contact_submit.php',
-      data:   info,
+      type:  'post',
+      url:   '/php/contact_submit.php',
+      data:  {
+        firstname:   $("#contact-first-name").val(),
+        lastname:    $("#contact-last-name").val(),
+        email:       $("#contact-email").val(), 
+        phone:       $("#contact-phone").val(),
+        project:     $("#contact-project").val(),
+        find:        $("#contact-find-us").val()
+      },
       success: function(response) {
         setTimeout(function() {
           $("#server-response").text(response);         
         }, 500);
       }
-    });  
+    }); 
+
   });
+
 });
-/*
- ██████╗██████╗  █████╗ ███████╗████████╗███████╗██████╗     ██████╗ ██╗   ██╗                               
-██╔════╝██╔══██╗██╔══██╗██╔════╝╚══██╔══╝██╔════╝██╔══██╗    ██╔══██╗╚██╗ ██╔╝                               
-██║     ██████╔╝███████║█████╗     ██║   █████╗  ██║  ██║    ██████╔╝ ╚████╔╝                                
-██║     ██╔══██╗██╔══██║██╔══╝     ██║   ██╔══╝  ██║  ██║    ██╔══██╗  ╚██╔╝                                 
-╚██████╗██║  ██║██║  ██║██║        ██║   ███████╗██████╔╝    ██████╔╝   ██║                                  
- ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝        ╚═╝   ╚══════╝╚═════╝     ╚═════╝    ╚═╝                                  
-                                                                                                             
-██╗   ██╗██████╗ ███████╗████████╗ █████╗ ██████╗ ████████╗██████╗ ██╗   ██╗██████╗ ███████╗ █████╗ ██╗   ██╗
-██║   ██║██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██║   ██║██╔══██╗██╔════╝██╔══██╗██║   ██║
-██║   ██║██████╔╝███████╗   ██║   ███████║██████╔╝   ██║   ██████╔╝██║   ██║██████╔╝█████╗  ███████║██║   ██║
-██║   ██║██╔═══╝ ╚════██║   ██║   ██╔══██║██╔══██╗   ██║   ██╔══██╗██║   ██║██╔══██╗██╔══╝  ██╔══██║██║   ██║
-╚██████╔╝██║     ███████║   ██║   ██║  ██║██║  ██║   ██║   ██████╔╝╚██████╔╝██║  ██║███████╗██║  ██║╚██████╔╝
- ╚═════╝ ╚═╝     ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ ╚═════╝ 
-
-copyright: 2015
-*/
-
-  (function() {
-
-    // when page loads, add all scrollable elements to the array
-    var elements = [];
-    $('.scroll-fast, .scroll-fast-left, .scroll-fast-right, ' + 
-      '.scroll-medium, .scroll-medium-left, .scroll-medium-right, ' + 
-      '.scroll-slow, .scroll-slow-left, .scroll-slow-right, ' + 
-      '.scroll-fast-down, .scroll-medium-down, .scroll-slow-down').each(function() {
-
-      var elem = $(this);
-
-      // set the scrollStyle property of the element
-      if (elem.hasClass('scroll-fast-left') || 
-          elem.hasClass('scroll-medium-left') ||
-          elem.hasClass('scroll-slow-left'))
-        elem.scrollStyle = 'left';
-      else if (elem.hasClass('scroll-fast-right') || 
-               elem.hasClass('scroll-medium-right') ||
-               elem.hasClass('scroll-slow-right'))
-        elem.scrollStyle = 'right';
-      else if (elem.hasClass('scroll-fast-down') || 
-               elem.hasClass('scroll-medium-down') ||
-               elem.hasClass('scroll-slow-down'))
-        elem.scrollStyle = 'down';
-      else
-        elem.scrollStyle = 'normal';
-
-      // add it to the array
-      elements.push(elem);
-    });
-
-    function elementPosition(elem) {
-      if (elem.offset().top + elem.height() < $(window).scrollTop() - 100) {
-        return 'above';
-      } else if (elem.offset().top > $(window).scrollTop() + $(window).height() /*- 100*/) {
-        return 'below';
-      } else {
-        return 'within';
-      }
-    }
-
-    // when the page scrolls, animate the elements depending on the scroll speed and style
-    $('body').scroll(function(e) {
-      for (var i = 0; i < elements.length; i++) {
-
-        switch(elements[i].scrollStyle) {
-
-          case 'normal':
-            switch(elementPosition(elements[i])) {
-              case 'above':
-                elements[i].addClass('move-down');
-                break;
-              case 'below':
-                elements[i].addClass('move-up');
-                break;
-              case 'within':
-                elements[i].removeClass('move-down').removeClass('move-up');
-                break;
-            }   
-            break; 
-
-          case 'left':
-            switch(elementPosition(elements[i])) {
-              case 'above': 
-              case 'below':
-                elements[i].addClass('move-left');
-                break;
-              case 'within':
-                elements[i].removeClass('move-left');
-                break;
-            } 
-            break;
-
-          case 'right':
-            switch(elementPosition(elements[i])) {
-              case 'above':
-              case 'below':
-                elements[i].addClass('move-right');
-                break;
-              case 'within':
-                elements[i].removeClass('move-right');
-                break;
-            } 
-            break; 
-
-          case 'down':
-            switch(elementPosition(elements[i])) {
-              case 'above':
-              case 'below':
-                elements[i].addClass('move-down');
-                break;
-              case 'within':
-                elements[i].removeClass('move-down');
-                break;
-            } 
-            break; 
-
-        }
-
-      }
-    });
-
-  })();
