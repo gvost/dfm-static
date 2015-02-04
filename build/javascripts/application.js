2fe124b11520719f83ed1720c9588e7e4c136bd6
Jordan <jordan314@yahoo.com>
2015-02-04 15:18:45 -0800

    removing 'find' from form logic

--- build/javascripts/application.js
+++ build/javascripts/application.js
@@ -408,7 +408,7 @@ $(document).ready(function() {
       email:       $("#contact-email").val(), 
       phone:       $("#contact-phone").val(),
       project:     $("#contact-project").val(),
-      find:        $("#contact-find-us").val(),
+      //find:        $("#contact-find-us").val(),
       nr:          $("#nr").val(),
     };
 
